2d70d304bd5deba00a9a67b2032b8f2dec6fc4a4
Brittany Morris <brittany@curiouser.ai>
2025-04-12 22:47:07 -0400

    fixing api

--- src/components/MiningMap.jsx
+++ src/components/MiningMap.jsx
@@ -1,3 +1,12 @@
+import React from 'react';
+import { GoogleMap, LoadScript, Marker, InfoWindow } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '100%',
+  height: '400px',
+  marginTop: '20px'
+};
+
 const MiningMap = ({ sites, onLocationSelect }) => {
   const [selectedSite, setSelectedSite] = React.useState(null);
   const center = sites.length ? { lat: sites[0].lat, lng: sites[0].lng } : { lat: 20, lng: 0 };
@@ -49,3 +58,5 @@ const MiningMap = ({ sites, onLocationSelect }) => {
     </LoadScript>
   );
 };
+
+export default MiningMap;
